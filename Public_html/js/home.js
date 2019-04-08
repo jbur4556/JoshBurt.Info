0e093d2a6d78b9e7154bf475293e632b3ce44432
Josh Burt <jburt@bosotability.com>
2019-04-08 08:22:37 -0600

    Changed drop down to inline

--- Public_html/js/home.js
+++ Public_html/js/home.js
@@ -1,6 +1,11 @@
 function goToSection(id) //scrolls Bottum of section to Bottum of screen
 {
-	var element = document.getElementById(id)
+	var element = document.getElementById(id);
 
 	element.scrollIntoView(false);
+}
+
+function buttonClassToggle(id,class1,class2)
+{
+	var element = document.getElementById(id);	
 }
