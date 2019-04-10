071cbd69fdf23b997433f15c64260ef445db9466
Josh Burt <jburt@bosotability.com>
2019-04-10 16:00:16 -0600

    Started Underline Work

--- Public_html/js/home.js
+++ Public_html/js/home.js
@@ -1,3 +1,5 @@
+window.addEventListener("scroll",moveHeaderUnderline);
+
 function goToSection(id) //scrolls Bottum of section to Bottum of screen
 {
 	var element = document.getElementById(id);
@@ -5,7 +7,43 @@ function goToSection(id) //scrolls Bottum of section to Bottum of screen
 	element.scrollIntoView(false);
 }
 
-function buttonClassToggle(id,class1,class2)
+function ShowHeaderLinks(toShow,toHide,class1,class2)
 {
-	var element = document.getElementById(id);	
+	var e = document.getElementById(toShow);
+	var el = document.getElementById(toHide);	
+	
+	e.classList.toggle(class1);
+	el.classList.toggle(class2);
+	
 }
+
+function moveHeaderUnderline()
+{
+	const elementStyle = document.getElementById("underline").style;
+	
+	var h = document.documentElement, 
+        b = document.body,
+        st = 'scrollTop',
+        sh = 'scrollHeight';
+    var percentage=(h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight);
+	var lerpPercentage=66.66*percentage;
+	
+	elementStyle.marginLeft=Math.ceil(lerpPercentage) + '%'
+	
+	console.log(Math.ceil(lerpPercentage) + '%');
+}
+
+/*function buttonClassToggle(id,class1,class2)
+{
+	var el = document.getElementById(id);	
+	
+	if(el.className=class1)
+	{
+		el.classList.remove(class1);
+		el.classList.add(class2);
+	}
+	if(el.className=class2)
+	{
+		el.className=class1;
+	}
+}*/
