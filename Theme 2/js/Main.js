660714034914a04db46948df36bf085f97149d70
Josh Burt <jburt@bosotability.com>
2019-05-13 09:54:11 -0600

    Added Highlight Function to Scroll in JS

--- Theme 2/js/Main.js
+++ Theme 2/js/Main.js
@@ -1,3 +1,6 @@
+var BGColor = "#fafafa"
+var BGHighlightColor = "#aaaaaa";
+
 function clickSection()
 {
 	window.location.href = ""//needs URL/Direction
@@ -6,6 +9,17 @@ function clickSection()
 function goToSection(id) //scrolls Bottum of section to Bottum of screen
 {
 	var element = document.getElementById(id);
-
 	element.scrollIntoView(true);
+
+	BGHilight(element);
+
+	/*var col = element.style.backgroundColor;
+	element.style.backgroundColor = "red";
+	setTimeout(function(){element.style.backgroundColor = col;}, 1000);*/
+} 
+
+function BGHilight(el)
+{
+	el.style.backgroundColor = BGHighlightColor;
+	setTimeout(function(){el.style.backgroundColor = BGColor;}, 500);
 }
